023de48c90a8545e8c5fc5dc0295c8d6d96f691c
Moises <grifincaromiranda00@gmail.com>
2024-10-15 21:36:56 -0400

    primera version

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        # Aquí puedes manejar el envío de datos, almacenarlos o enviarlos por correo.
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
